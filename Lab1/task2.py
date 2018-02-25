5191059ceb6ae14f694ba770490255ab8ceac22d
Piotr Grzybowski <p.grzybowski2@gmail.com>
2018-02-25 20:40:46 +0100

    All tasks from lab1 done

--- Lab1/task2.py
+++ Lab1/task2.py
@@ -1,57 +1,32 @@
-# import numpy as np
-# from matplotlib import pyplot as plt
-# from matplotlib import animation
-#
-# from Lab1.utils import integer_generator_from_range
-#
-# start = 0
-# stop = 10
-#
-# fig = plt.figure()
-# occurrences = np.zeros((stop, stop))
-# im = plt.imshow(occurrences, cmap=plt.get_cmap('plasma'))
-#
-#
-# def init():
-#     im.set_data(np.zeros((start, stop)))
-#
-#
-# def animate(i):
-#     first = next(integer_generator_from_range(start, stop))
-#     second = next(integer_generator_from_range(start, stop))
-#     occurrences[first][second] += 1
-#     im.set_data(occurrences)
-#     return im
-#
-#
-# anim = animation.FuncAnimation(fig, animate, init_func=init, frames=50, repeat=False)
-# plt.show()
-
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
-nx = 10
-ny = 10
-start = 0
-stop = 10
+from Lab1.utils import integer_generator_from_range
 
-fig = plt.figure()
-data = np.random.rand(nx, ny)
-im = plt.imshow(data, cmap=plt.get_cmap('plasma'))
+start = 0
+stop = 5
+occurrences = np.zeros((stop, stop))
+
+fig, ax = plt.subplots()
+data = np.random.rand(stop, stop)
+im = ax.imshow(data, cmap=plt.get_cmap('hsv'))
+
+ax.set_xlim(start-1, stop)
+ax.set_ylim(start-1, stop)
 
 
 def init():
-    im.set_data(np.zeros((nx, ny)))
+    im.set_data(np.zeros((stop, stop)))
 
 
 def animate(i):
-    # xi = i // ny
-    # yi = i % ny
-    data = np.random.rand(nx, ny)
-    im.set_data(data)
+    first = next(integer_generator_from_range(start, stop))
+    second = next(integer_generator_from_range(start, stop))
+    occurrences[first][second] += 1
+    im.set_data(occurrences / np.sum(occurrences))
     return im
 
 
-anim = animation.FuncAnimation(fig, animate, init_func=init, frames=50, repeat=False)
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames=200, interval=100, repeat=False)
 plt.show()
