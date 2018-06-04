591f4dc1b0f80195cd342ac572175063eab63229
Piotr Grzybowski <p.grzybowski2@gmail.com>
2018-06-04 04:41:00 +0200

    Done for visuali

--- Lab10/graph_utils.py
+++ Lab10/graph_utils.py
@@ -7,8 +7,21 @@ HEAD_TO_TAIL = 0
 
 
 def is_path_blocked_by_observation(graph, path, observation):
+    segments = list(get_segments_from_path(path))
+    if len(segments) == 0:
+        return False
+
     for segment in get_segments_from_path(path):
-        pass
+        segment_type = get_segment_type(graph, segment)
+
+        if segment_type == HEAD_TO_HEAD:
+            if segment[1] not in observation and len(get_descendants(graph, segment[1]) & observation) == 0:
+                return True
+        elif segment_type == HEAD_TO_TAIL or segment_type == TAIL_TO_TAIL:
+            if segment[1] in observation:
+                return True
+
+    return False
 
 
 def get_segment_type(graph, segment):
@@ -37,3 +50,11 @@ def get_descendants(graph, source):
 def generate_random_dag(n, p):
     random_graph = nx.fast_gnp_random_graph(n, p, directed=True)
     return nx.DiGraph([(u, v) for (u, v) in random_graph.edges() if u < v])
+
+
+def are_nodes_d_separated_by_observation(graph, source, target, observation):
+    for path in nx.all_simple_paths(graph.to_undirected(), source, target):
+        if not is_path_blocked_by_observation(graph, path, observation):
+            return False
+
+    return True
--- Lab10/test_graph_utils/test_graph_utils.py
+++ Lab10/test_graph_utils/test_graph_utils.py
@@ -2,7 +2,7 @@ import networkx as nx
 import unittest
 
 from Lab10.graph_utils import TAIL_TO_TAIL, get_segment_type, HEAD_TO_HEAD, HEAD_TO_TAIL, get_segments_from_path, \
-    generate_random_dag, get_descendants
+    generate_random_dag, get_descendants, is_path_blocked_by_observation, are_nodes_d_separated_by_observation
 
 
 class TestGraphUtils(unittest.TestCase):
@@ -57,6 +57,71 @@ class TestGraphUtils(unittest.TestCase):
 
     def test_generate_random_dag(self):
         self.assertTrue(nx.is_directed_acyclic_graph(generate_random_dag(10, 0.5)))
+
+    def test_is_path_blocked_by_observation(self):
+        graph = nx.DiGraph()
+
+        graph.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8])
+        graph.add_edges_from([(1, 3),
+                              (2, 3),
+                              (3, 4),
+                              (3, 8),
+                              (4, 6),
+                              (5, 6),
+                              (6, 7)])
+
+        self.assertEqual(is_path_blocked_by_observation(graph, path=[1, 3, 2], observation={3}), False)
+        self.assertEqual(is_path_blocked_by_observation(graph, path=[2, 3, 1], observation={3}), False)
+        self.assertEqual(is_path_blocked_by_observation(graph, path=[1, 3, 4], observation={3}), True)
+        self.assertEqual(is_path_blocked_by_observation(graph, path=[4, 3, 8], observation={3}), True)
+        self.assertEqual(is_path_blocked_by_observation(graph, path=[2, 3, 4], observation={3}), True)
+        self.assertEqual(is_path_blocked_by_observation(graph, path=[4, 3, 2], observation={3}), True)
+        self.assertEqual(is_path_blocked_by_observation(graph, path=[5, 6, 4], observation={3}), True)
+        self.assertEqual(is_path_blocked_by_observation(graph, path=[4, 6], observation={3}), False)
+
+    def test_are_nodes_d_separated_by_observation(self):
+        graph = nx.DiGraph()
+
+        graph.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8])
+        graph.add_edges_from([(1, 3),
+                              (2, 3),
+                              (3, 4),
+                              (3, 8),
+                              (4, 6),
+                              (5, 6),
+                              (6, 7)])
+
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 1, 4, {3}), True)
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 1, 2, {3}), False)
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 4, 5, {3}), True)
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 4, 7, {3}), False)
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 4, 8, {3}), True)
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 4, 6, {3}), False)
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 2, 7, {3}), True)
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 2, 5, {3}), True)
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 5, 8, {3}), True)
+
+    def test_are_nodes_d_separated_by_observation_2(self):
+        graph = nx.DiGraph()
+
+        graph.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8])
+        graph.add_edges_from([(1, 3),
+                              (2, 3),
+                              (3, 4),
+                              (3, 8),
+                              (4, 6),
+                              (5, 6),
+                              (6, 7)])
+
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 1, 4, {7}), False)
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 1, 2, {7}), False)
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 4, 5, {7}), False)
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 4, 7, {7}), False)
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 4, 8, {7}), False)
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 4, 6, {7}), False)
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 2, 7, {7}), False)
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 2, 5, {7}), False)
+        self.assertEqual(are_nodes_d_separated_by_observation(graph, 5, 8, {7}), False)
 
 
 if __name__ == '__main__':
